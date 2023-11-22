b1e77fd5bbb23773f9711ac4572a21b780263007
Michael Thomas <michaelhthomas@outlook.com>
2023-11-21 22:53:04 -0500

    fix(click-on): use idiomatic promises, improve typescript

--- src/nodes/click-on.ts
+++ src/nodes/click-on.ts
@@ -1,12 +1,13 @@
+import { NodeMessage } from "node-red";
 import { WD2Manager } from "../wd2-manager";
 import {
 	SeleniumAction,
 	SeleniumMsg,
 	SeleniumNode,
 	SeleniumNodeDef,
-	waitForElement,
 } from "./node";
 import { GenericSeleniumConstructor } from "./node-constructor";
+import { isError } from "../utils";
 
 export interface NodeClickOnDef extends SeleniumNodeDef {
 	clickOn?: boolean;
@@ -21,41 +22,39 @@ async function inputPreCondAction(
 	conf: NodeClickOnDef,
 	action: SeleniumAction
 ): Promise<boolean> {
-	return new Promise<boolean>(async (resolve, reject) => {
-		let msg = action.msg;
-		if (msg.click && node.__msg) {
-			msg = node.__msg; // msg restoration
-			try {
-				await msg.element.click();
-				node.status({ fill: "green", shape: "dot", text: "success" });
-				if (msg.error) {
-					delete msg.error;
-				}
-				action.send([msg, null]);
-				action.done();
-			} catch (err) {
-				if (WD2Manager.checkIfCritical(err)) {
-					reject(err);
-				} else {
-					msg.error = {
-						value: "Can't click on the the element : " + err.message,
-					};
-					node.status({ fill: "yellow", shape: "dot", text: "click error" });
-					action.send([null, msg]);
-					action.done();
-				}
+	let msg = action.msg;
+	if (msg.click && node.__msg) {
+		msg = node.__msg; // msg restoration
+		try {
+			await msg.element.click();
+			node.status({ fill: "green", shape: "dot", text: "success" });
+			if (msg.error) {
+				delete msg.error;
 			}
-			resolve(false); // We don't want to execute the full node
+			action.send([msg, null]);
+			action.done();
+		} catch (err) {
+			if (!isError(err) || WD2Manager.checkIfCritical(err)) {
+				throw err;
+			} else {
+				msg.error = {
+					value: "Can't click on the the element: " + err.message,
+				};
+				node.status({ fill: "yellow", shape: "dot", text: "click error" });
+				action.send([null, msg]);
+				action.done();
+			}
 		}
-		if (msg.click && !node.__msg) {
-			node.status({ fill: "yellow", shape: "ring", text: "ignored" });
-			setTimeout(() => {
-				node.status({});
-			}, 3000);
-			resolve(false);
-		}
-		resolve(true);
-	});
+		return false; // We don't want to execute the full node
+	}
+	if (msg.click && !node.__msg) {
+		node.status({ fill: "yellow", shape: "ring", text: "ignored" });
+		setTimeout(() => {
+			node.status({});
+		}, 3000);
+		return false;
+	}
+	return true;
 }
 
 async function inputAction(
@@ -64,39 +63,37 @@ async function inputAction(
 	action: SeleniumAction
 ): Promise<void> {
 	const msg = action.msg;
-	return new Promise<void>(async (resolve, reject) => {
-		if (!conf.clickOn) {
-			try {
-				await msg.element.click();
-				node.status({ fill: "green", shape: "dot", text: "success" });
-				if (msg.error) {
-					delete msg.error;
-				}
-				action.send([msg, null]);
-				action.done();
-			} catch (err) {
-				if (WD2Manager.checkIfCritical(err)) {
-					reject(err);
-				} else {
-					msg.error = {
-						value: "Can't click on the the element : " + err.message,
-					};
-					node.status({ fill: "yellow", shape: "dot", text: "click error" });
-					action.send([null, msg]);
-					action.done();
-				}
+
+	if (!conf.clickOn) {
+		try {
+			await msg.element.click();
+			node.status({ fill: "green", shape: "dot", text: "success" });
+			if (msg.error) {
+				delete msg.error;
 			}
-		} else {
-			// If we have to wait for the user click and we save the msg
-			node.status({
-				fill: "blue",
-				shape: "dot",
-				text: "waiting for user click",
-			});
-			node.__msg = msg;
+			action.send([msg, null]);
+			action.done();
+		} catch (err) {
+			if (!isError(err) || WD2Manager.checkIfCritical(err)) {
+				throw err;
+			} else {
+				msg.error = {
+					value: "Can't click on the the element : " + err.message,
+				};
+				node.status({ fill: "yellow", shape: "dot", text: "click error" });
+				action.send([null, msg]);
+				action.done();
+			}
 		}
-		resolve();
-	});
+	} else {
+		// If we have to wait for the user click and we save the msg
+		node.status({
+			fill: "blue",
+			shape: "dot",
+			text: "waiting for user click",
+		});
+		node.__msg = msg;
+	}
 }
 
 const NodeClickOnConstructor = GenericSeleniumConstructor(
@@ -114,16 +111,16 @@ export function NodeClickPrerequisite() {
 			const node = WD2Manager.RED.nodes.getNode(req.params.id);
 			if (node != null) {
 				try {
-					// @ts-ignore
-					node.receive({ click: true });
+					node.receive({ click: true } as NodeMessage);
 					res.sendStatus(200);
 				} catch (err) {
 					res.sendStatus(500);
-					node.error(
-						WD2Manager.RED._("inject.failed", {
-							error: err.toString(),
-						})
-					);
+					if (isError(err))
+						node.error(
+							WD2Manager.RED._("inject.failed", {
+								error: err.toString(),
+							})
+						);
 				}
 			} else {
 				res.sendStatus(404);
